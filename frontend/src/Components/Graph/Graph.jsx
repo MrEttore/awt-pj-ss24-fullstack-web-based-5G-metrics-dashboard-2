a18346ff90479f90760d9d4c6fd619cc482b59db
Ettore Marangon <marangonettore@gmail.com>
2024-05-20 20:56:32 +0200

    added dummy graphs with random data

--- frontend/src/Components/Graph/Graph.jsx
+++ frontend/src/Components/Graph/Graph.jsx
@@ -1,5 +1,66 @@
 import "./Graph.css";
+import { Line } from "react-chartjs-2";
+import { faker } from "@faker-js/faker";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+// Register Chart.js components and scales
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Dataset 2",
+      data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
+const options = {
+  responsive: true,
+
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: true,
+      text: "Dummy graph",
+    },
+  },
+};
 
 export default function Graph() {
-  return <div className="graph">GraphX</div>;
+  return (
+    <div className="graph">
+      <Line options={options} data={data} />
+    </div>
+  );
 }
