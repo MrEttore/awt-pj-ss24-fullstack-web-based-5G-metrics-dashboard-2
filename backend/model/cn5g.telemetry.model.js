26356792fb3625fd60957ad5fd8cc4c873c42ed7
dschrenk <d.schrenk@campus.tu-berlin.de>
2024-05-26 15:35:02 +0200

    Implement cn5g.telemetry API

---
+++ backend/model/cn5g.telemetry.model.js
@@ -0,0 +1,140 @@
+
+const {
+    INIT,
+    SELECT,
+    INSERT
+} = require('../sql/cn5g.telemetry.sql')
+
+
+/*
+*   Persistence API for managing cn5g.telemetry data
+*/
+class Cn5gTelemetryModel {
+
+    #db;
+
+    constructor() {
+        this.#db = require('../database/sqlite3');
+        this.#init()
+    }
+
+    /*
+    *   Initialize database
+    */
+    #init() {
+        this.#db.run(INIT)
+    }
+
+    /*
+    *   Get entry by ID
+    *   Param: id
+    *   Returns row in JSON format or undefined
+    */
+    async get(id) {
+        const QUERY = `
+            ${SELECT}
+            WHERE rowId = ?`
+
+        return new Promise((resolve, reject) => {
+            this.#db.get(QUERY, [id], (err, row) => {
+                if (err)
+                    reject(err)
+                else
+                    resolve(row ? JSON.parse(
+                        row.jsonData
+                    ) : row
+                    )
+            })
+        })
+    }
+
+    /*
+    *   Get all matching entries
+    *   Params: timeStart, timeEnd
+    */
+    async getAll(params) {
+        let query = SELECT
+        const {
+            timeStart,
+            timeEnd,
+        } = params
+
+        let paramList = []
+        if (timeStart && timeEnd && parseInt(timeStart) !== NaN && parseInt(timeEnd) !== NaN) {
+            query += ' WHERE timeStart <= ? AND ? <= timeEnd'
+            paramList.push(timeStart, timeEnd)
+        }
+
+        return new Promise((resolve, reject) => {
+            this.#db.all(query, paramList, (err, rows) => {
+                if (err)
+                    reject(err)
+                else
+                    resolve(rows
+                        .map(row => row.jsonData)
+                        .map(JSON.parse)
+                    )
+            })
+        })
+
+    }
+
+    /*
+    *   Insert new entry
+    *   Returns ID of new entry
+    */
+    async add(data) {
+        const {
+            timestamp,
+            amf: { status: amfStatus, message: amfMessage },
+            ausf: { status: ausfStatus, message: ausfMessage },
+            nrf: { status: nrfStatus, message: nrfMessage },
+            smf: { status: smfStatus, message: smfMessage },
+            upf: { status: upfStatus, message: upfMessage },
+            udm: { status: udmStatus, message: udmMessage },
+            udr: { status: udrStatus, message: udrMessage },
+            nssf: { status: nssfStatus, message: nssfMessage },
+            nef: { status: nefStatus, message: nefMessage },
+            mysql: { status: mysqlStatus, message: mysqlMessage },
+            extdn: { status: extdnStatus, message: extdnMessage },
+            asterisk: { status: asteriskStatus, message: asteriskMessage },
+            openspeedtest: { status: openspeedtestStatus, message: openspeedtestMessage },
+            oaiExtDnUplinkState,
+            oaiExtDnDownlinkInstances
+        } = data;
+
+        return new Promise((resolve, reject) => {
+            this.#db.run(INSERT, [
+                timestamp,
+                amfStatus, amfMessage,
+                ausfStatus, ausfMessage,
+                nrfStatus, nrfMessage,
+                smfStatus, smfMessage,
+                upfStatus, upfMessage,
+                udmStatus, udmMessage,
+                udrStatus, udrMessage,
+                nssfStatus, nssfMessage,
+                nefStatus, nefMessage,
+                mysqlStatus, mysqlMessage,
+                extdnStatus, extdnMessage,
+                asteriskStatus, asteriskMessage,
+                openspeedtestStatus, openspeedtestMessage,
+                oaiExtDnUplinkState, oaiExtDnDownlinkInstances
+            ], function (err) {
+                if (err)
+                    reject(err)
+                else
+                    resolve(this.lastID)
+            })
+        })
+
+    }
+
+    async delete(id) {
+        // Implement delete logic if needed
+    }
+
+}
+
+const INSTANCE = new Cn5gTelemetryModel();
+module.exports = INSTANCE;
---
+++ backend/router/cn5g.telemetry.router.js
@@ -0,0 +1,27 @@
+
+const express = require('express')
+
+const router = express.Router()
+
+const model = require('../model/cn5g.telemetry.model')
+
+router.get('/', async (req, res) => {
+    const rows = await model.getAll({...req.params})
+    for (let row of rows) {
+        row.href = req.originalUrl+ '/' + row.rowId
+    }
+    res.json(rows)
+})
+
+router.get('/:id', async (req, res) => {
+    const id = parseInt(req.params.id)
+
+    const row = await model.get(id)
+    if (!row) {
+        return res.status(400).send();
+    }
+    row.href = req.originalUrl;
+    res.json(row);
+})
+
+module.exports = router;
