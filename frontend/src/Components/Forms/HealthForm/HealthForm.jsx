85c6ce64094c5652de8b1cf9fb5733cf3563436f
Ettore Marangon <marangonettore@gmail.com>
2024-05-31 16:22:53 +0200

    added Form for Health stats

---
+++ frontend/src/Components/Forms/HealthForm/HealthForm.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import "../Forms.css";
+
+export default function HealthForm({ selectedTab }) {
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+
+  function handleReset() {
+    setStartTime("");
+    setEndTime("");
+  }
+
+  function handleSubmit(e) {
+    // prevent the page to reload
+    e.preventDefault();
+
+    const timestamp = {
+      startTime: startTime,
+      endTime: endTime,
+    };
+
+    console.log(timestamp);
+
+    // TODO: API call
+  }
+
+  return (
+    <form className={`${selectedTab}Form`} onSubmit={handleSubmit}>
+      <label htmlFor="time">Select time</label>
+      <div className="inputContainer">
+        <input
+          type="text"
+          id="time"
+          placeholder="From ..."
+          value={startTime}
+          onChange={(e) => {
+            setStartTime(Number(e.target.value));
+          }}
+          required
+        />
+        <input
+          type="text"
+          id="time"
+          placeholder="To ..."
+          value={endTime}
+          onChange={(e) => {
+            setEndTime(Number(e.target.value));
+          }}
+          required
+        />
+      </div>
+
+      <div className="btnContainer">
+        <button className="btnSubmit" type="submit">
+          Submit
+        </button>
+        <button className="btnReset" onClick={handleReset}>
+          Reset
+        </button>
+      </div>
+    </form>
+  );
+}
