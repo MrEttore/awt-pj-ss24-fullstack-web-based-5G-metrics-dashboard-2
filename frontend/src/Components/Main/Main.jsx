9fa89e65878e7a0e50b66b93390e3bb5963c01db
Ettore Marangon <marangonettore@gmail.com>
2024-05-31 12:19:27 +0200

    lifted state from Tab component

--- frontend/src/Components/Main/Main.jsx
+++ frontend/src/Components/Main/Main.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import ControlBar from "../ControlBar/ControlBar.jsx";
-import Graphs from "../Graphs/Graphs.jsx";
+import Content from "../Content/Content.jsx";
 import "./Main.css";
 
 function Main() {
+  // piece of state to control the tab selection
+  const [selectedTab, setSelectedTab] = useState("healthStatus");
+
+  function handleSelectedTab(e) {
+    setSelectedTab(e.target.value);
+  }
+
   return (
     <main className="main">
-      <ControlBar />
-      {/* <Graphs /> */}
+      {/* Tabs and forms */}
+      <ControlBar selectedTab={selectedTab} onSelect={handleSelectedTab} />
+
+      {/* Content */}
+      {/* <Content /> */}
     </main>
   );
 }
