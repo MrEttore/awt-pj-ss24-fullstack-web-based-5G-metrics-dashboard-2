ee6ddda1fbd466888ab4f4b07c9b46bc244ea32a
Ettore Marangon <marangonettore@gmail.com>
2024-06-01 20:57:44 +0200

    added components for display purpose

---
+++ frontend/src/Components/Content/DisplayHealth/DisplayHealth.jsx
@@ -0,0 +1,9 @@
+import "./DisplayHealth.css";
+
+export default function DisplayHealth() {
+  return (
+    <div className="contentHealth">
+      <p>Health</p>
+    </div>
+  );
+}
---
+++ frontend/src/Components/Content/DisplayLogs/DisplayLogs.jsx
@@ -0,0 +1,9 @@
+import "./DisplayLogs.css";
+
+export default function DisplayLogs() {
+  return (
+    <div className="contentLogs">
+      <p>Logs</p>
+    </div>
+  );
+}
