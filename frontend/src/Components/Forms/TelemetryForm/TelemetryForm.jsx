e5594cc1716ca5f00efb94880032e73438c68a6a
Ettore Marangon <marangonettore@gmail.com>
2024-06-01 20:55:07 +0200

    updated Telemetry comp.

--- frontend/src/Components/Forms/TelemetryForm/TelemetryForm.jsx
+++ frontend/src/Components/Forms/TelemetryForm/TelemetryForm.jsx
@@ -21,56 +21,111 @@ const optionsDevices = [
 
 export default function TelemetryForm({ selectedTab }) {
   // piece of state to control the device selection
-  const [selectedDevice, setSelectedDevice] = useState("iPhone15");
+  const [selectedDevice, setSelectedDevice] = useState("");
+
+  // pieces of state for the timespan inputs
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+
+  // piece of state for the toggle button
+
+  function handleSelect(e) {}
+
+  function handleReset() {
+    setStartTime("");
+    setEndTime("");
+  }
+
+  function handleSubmit(e) {
+    // prevent the page to reload
+    e.preventDefault();
+
+    const timestamp = {
+      startTime: startTime,
+      endTime: endTime,
+      device: selectedDevice,
+    };
+
+    console.log(timestamp);
+
+    // TODO: API call
+  }
 
   return (
-    <form
-      className={`${selectedTab}Form`} /*  onSubmit={handleSubmit(event)} ... */
-    >
+    <form className={`${selectedTab}Form`} onSubmit={handleSubmit}>
+      {/* TIME */}
+      <div className="timeSpanContainer">
+        <label htmlFor="time">Select time</label>
+        <div className="inputContainer">
+          <input
+            className="time"
+            type="text"
+            id="time"
+            placeholder="From ..."
+            value={startTime}
+            onChange={(e) => {
+              setStartTime(Number(e.target.value));
+            }}
+            required
+          />
+          <input
+            className="time"
+            type="text"
+            id="time"
+            placeholder="To ..."
+            value={endTime}
+            onChange={(e) => {
+              setEndTime(Number(e.target.value));
+            }}
+            required
+          />
+        </div>
+      </div>
+
       {/* DEVICE */}
-      <label htmlFor="selectDevice">Select device</label>
-      <Select
-        className="selectDevice"
-        classNamePrefix="react-select"
-        id="selectDevice"
-        unstyled
-        isMulti
-        isSearchable
-        defaultValue={selectedDevice}
-        onChange={setSelectedDevice}
-        options={optionsDevices}
-        required
-      />
+      <div className="selectDeviceContainer">
+        <label htmlFor="selectDevice">Select device</label>
+        <Select
+          className="selectDevice"
+          classNamePrefix="react-select"
+          id="selectDevice"
+          unstyled
+          isMulti
+          isSearchable
+          // defaultValue={selectedDevice}
+          // value={selectedDevice}
+          onChange={(e) => {
+            console.log(e);
+          }}
+          options={optionsDevices}
+          required
+        />
+      </div>
 
       {/* METRICS */}
-      <label htmlFor="selectMetrics">Select Metric</label>
-      <Select
-        className="selectMetrics"
-        classNamePrefix="react-select"
-        id="selectMetrics"
-        unstyled
-        isMulti
-        isSearchable
-        defaultValue={selectedDevice}
-        onChange={setSelectedDevice}
-        options={optionsMetrics}
-        required
-      />
+      <div className="selectMetricsContainer">
+        <label htmlFor="selectMetrics">Select Metric</label>
+        <Select
+          className="selectMetrics"
+          classNamePrefix="react-select"
+          id="selectMetrics"
+          unstyled
+          isMulti
+          isSearchable
+          // defaultValue={selectedDevice}
+          onChange={setSelectedDevice}
+          options={optionsMetrics}
+          required
+        />
+      </div>
 
-      {/* TIME */}
-      <label htmlFor="time">Select time</label>
-      <input
-        className="time"
-        type="text"
-        id="time"
-        placeholder="Add an unix timestamp"
-        required
-      ></input>
       <div className="btnContainer">
         <button className="btnSubmit" type="submit">
           Submit
         </button>
-        <button className="btnReset">Reset</button>
+        <button className="btnReset" onClick={handleReset}>
+          Reset
+        </button>
       </div>
     </form>
   );
