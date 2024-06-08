798c418b99bead3ed308b16dfb920633ce452389
Ettore Marangon <marangonettore@gmail.com>
2024-06-08 18:10:58 +0200

    added health grid component

--- frontend/src/Components/Content/DisplayHealth/DisplayHealth.jsx
+++ frontend/src/Components/Content/DisplayHealth/DisplayHealth.jsx
@@ -1,9 +1,27 @@
+import { HiOutlineStatusOnline } from "react-icons/hi";
+import { HiOutlineStatusOffline } from "react-icons/hi";
+import HealthItem from "../../HealthItem/HealthItem";
 import "./DisplayHealth.css";
+import { useState } from "react";
 
 export default function DisplayHealth() {
+  const [isOnline, setIsOnline] = useState(false);
+
+  // TODO: API call to set the status of each component
+  // ...
+
   return (
-    <div className="contentHealth">
-      <p>Health</p>
+    <div className="gridHealth">
+      {Array.from({ length: 13 }, (_, i) => (
+        <HealthItem
+          key={i}
+          icon={
+            isOnline ? <HiOutlineStatusOnline /> : <HiOutlineStatusOffline />
+          }
+          status={isOnline}
+          moduleName={`Module_${i + 1}`}
+        />
+      ))}
     </div>
   );
 }
