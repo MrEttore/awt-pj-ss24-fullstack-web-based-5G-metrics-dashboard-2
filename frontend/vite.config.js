094c4b2e0b1b4a7ed0925de96e4f50efedbddb9c
smo <moritz.schelten@fokus.fraunhofer.de>
2024-06-10 16:44:31 +0200

    dockerize and update urls

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://backend:3000",
         changeOrigin: true,
       },
     },
