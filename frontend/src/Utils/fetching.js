5612235bb2bd0320686aec11554ff49d0ec0cd03
Ettore Marangon <marangonettore@gmail.com>
2024-06-23 16:35:21 +0200

    added fn to fetch data from cn5g api

---
+++ frontend/src/Utils/fetching.js
@@ -0,0 +1,20 @@
+import { CN5G_BASE_URL } from './constants';
+
+// Fetch cn5g telemetry
+export async function getCn5gData(timeStart, timeEnd) {
+  if (timeStart && timeEnd) {
+    try {
+      const response = await fetch(
+        `${CN5G_BASE_URL}timeStart=${timeStart.toString()}&timeEnd=${timeEnd.toString()}`
+      );
+
+      if (!response.ok) throw new Error('Response not ok!');
+
+      const data = await response.json();
+
+      return data;
+    } catch (err) {
+      console.error(`Failed to fetch the data: ${err}`);
+    }
+  } else throw new Error('Select a valid start and endtime for the request!');
+}
