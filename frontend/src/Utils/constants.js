79b8633c3ad062c05ad8409e411db372ac3f18e3
Ettore Marangon <marangonettore@gmail.com>
2024-06-23 16:35:47 +0200

    added file w/ constants

---
+++ frontend/src/Utils/constants.js
@@ -0,0 +1,19 @@
+export const CN5G_MODULES = [
+  'amf',
+  'ausf',
+  'nrf',
+  'smf',
+  'upf',
+  'udm',
+  'udr',
+  'nssf',
+  'nef',
+  'mysql',
+  'extdn',
+  'asterisk',
+  'openspeedtest',
+];
+
+/// APIs ///
+
+export const CN5G_BASE_URL = 'http://localhost:5173/api/cn5g/telemetry?';
