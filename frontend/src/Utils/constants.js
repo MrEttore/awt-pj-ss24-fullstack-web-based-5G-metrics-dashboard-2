ef6d78ca3f2f642212ab64be124183e2014e6350
Ettore Marangon <marangonettore@gmail.com>
2024-07-01 18:24:29 +0200

    added dashboard metrics as const

--- frontend/src/Utils/constants.js
+++ frontend/src/Utils/constants.js
@@ -14,6 +14,28 @@ export const CN5G_MODULES = [
   'openspeedtest',
 ];
 
+export const DASHBOARD_METRICS = [
+  'rnti',
+  'inSync',
+  'dlBytes',
+  'dlMcs',
+  'dlBler',
+  'ulBytes',
+  'ulMcs',
+  'ulBler',
+  'ri',
+  'pmi',
+  'phr',
+  'pcmax',
+  'rsrq',
+  'sinr',
+  'rsrp',
+  'rssi',
+  'cqi',
+  'pucchSnr',
+  'puschSnr',
+];
+
 /// APIs ///
 
 export const CN5G_BASE_URL = 'http://localhost:5173/api/cn5g/telemetry?';
