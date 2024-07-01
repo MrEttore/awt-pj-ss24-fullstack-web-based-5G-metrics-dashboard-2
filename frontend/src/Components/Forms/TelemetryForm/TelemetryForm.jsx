260a4d1415f742e7bb4f6e384c267908ab3e889b
Ettore Marangon <marangonettore@gmail.com>
2024-07-01 18:38:07 +0200

    added dropdown menus to telemetry form

--- frontend/src/Components/Forms/TelemetryForm/TelemetryForm.jsx
+++ frontend/src/Components/Forms/TelemetryForm/TelemetryForm.jsx
@@ -1,23 +1,4 @@
-import { useState } from 'react';
-import Select from 'react-select';
 import '../Forms.css';
-
-const optionsMetrics = [
-  { value: 'dlBytes', label: 'DL Bytes' },
-  { value: 'ulBytes', label: 'UL Bytes' },
-];
-
-const optionsDevices = [
-  {
-    value: 'iPhone15',
-    label: 'iPhone15',
-  },
-
-  {
-    value: 'Samsung S24',
-    label: 'Samsung S24',
-  },
-];
 
 export default function TelemetryForm({
   selectedTab,
@@ -25,52 +6,8 @@ export default function TelemetryForm({
   onSubmit,
   children,
 }) {
-  // piece of state to control the device selection
-  const [selectedDevice, setSelectedDevice] = useState('');
-
-  // TODO: lift state
-  function handleSelect(e) {}
-
   return (
     <form className={`${selectedTab}Form`} onSubmit={onSubmit}>
-      {/* DEVICE */}
-      <div className="selectDeviceContainer">
-        <label htmlFor="selectDevice">Select device</label>
-        <Select
-          className="selectDevice"
-          classNamePrefix="react-select"
-          id="selectDevice"
-          unstyled
-          isMulti
-          isSearchable
-          // defaultValue={selectedDevice}
-          // value={selectedDevice}
-          onChange={(e) => {
-            console.log(e);
-          }}
-          options={optionsDevices}
-          required
-          isDisabled={isLiveDataOn}
-        />
-      </div>
-      {/* METRICS */}
-      <div className="selectMetricsContainer">
-        <label htmlFor="selectMetrics">Select Metric</label>
-        <Select
-          className="selectMetrics"
-          classNamePrefix="react-select"
-          id="selectMetrics"
-          unstyled
-          isMulti
-          isSearchable
-          // defaultValue={selectedDevice}
-          onChange={setSelectedDevice}
-          options={optionsMetrics}
-          required
-          isDisabled={isLiveDataOn}
-        />
-      </div>
-
       {/* TIME + LIVE DATA + METRICS + BTNS */}
       {children}
     </form>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "http://backend:3000",
+        target: 'http://localhost:3000', // "http://backend:3000",
         changeOrigin: true,
       },
     },
