f5c3341f457b14be404570c7467c66c50ac3d37e
Ettore Marangon <marangonettore@gmail.com>
2024-07-03 17:40:19 +0200

    added function to get devices for dropdown menu

--- frontend/src/Utils/fetching.js
+++ frontend/src/Utils/fetching.js
@@ -1,6 +1,6 @@
 import { CN5G_BASE_URL, GNB_LOGS_URL, GNB_TELEMETRY_URL } from './constants.js';
 
-// Fetch cn5g telemetry
+// Fetch cn5g telemetry using timespans
 export async function getCn5gData(timeStart, timeEnd) {
   if (timeStart && timeEnd) {
     try {
@@ -21,7 +21,7 @@ export async function getCn5gData(timeStart, timeEnd) {
   } else throw new Error('Select a valid start and endtime for the request!');
 }
 
-// Fetch gnb logs
+// Fetch gnb logs using timespans
 export async function getGnbLogs(timeStart, timeEnd) {
   if (timeStart && timeEnd) {
     try {
@@ -39,5 +39,20 @@ export async function getGnbLogs(timeStart, timeEnd) {
         `Failed to fetch the data inside getGnbLogs: ${err.message}`
       );
     }
+  }
+}
+
+// Fetch available ues (devices)
+export async function getGnbUes() {
+  try {
+    const response = await fetch(`${GNB_TELEMETRY_URL}?ues`);
+
+    if (!response.ok) throw new Error("API response not ok!'");
+
+    const data = await response.json();
+
+    return data;
+  } catch (err) {
+    console.error(`Failed to fetch the data inside getGnbUes: ${err.message}`);
   }
 }
