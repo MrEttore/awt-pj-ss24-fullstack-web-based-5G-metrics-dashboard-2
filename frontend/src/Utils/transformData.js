885c3adebe203d72adcf6f0e4dbcc35b8d1218f3
Ettore Marangon <marangonettore@gmail.com>
2024-07-05 15:45:57 +0200

    added function to prepare the telemetry data for the UI

--- frontend/src/Utils/transformData.js
+++ frontend/src/Utils/transformData.js
@@ -1,4 +1,4 @@
-import { CN5G_MODULES } from './constants';
+import { CN5G_MODULES, DASHBOARD_METRICS } from './constants.js';
 
 export function transformHealthData(data) {
   // Initialize the structure to store each module's data
@@ -21,7 +21,34 @@ export function transformHealthData(data) {
   // Convert the object to an array of objects
   const result = CN5G_MODULES.map((module) => ({
     moduleName: module,
-    data: moduleData[module],
+    moduleData: moduleData[module],
+  }));
+
+  return result;
+}
+
+export function transformTelemetryData(data) {
+  // Initialize the structure to store each module's data
+  const metricData = {};
+  DASHBOARD_METRICS.forEach((metric) => {
+    metricData[metric] = [];
+  });
+
+  // Process each record
+  data.forEach((record) => {
+    const timestamp = record.timestamp;
+    DASHBOARD_METRICS.forEach((metric) => {
+      if (record.ues[0].hasOwnProperty(metric)) {
+        const value = record.ues[0][metric];
+        metricData[metric].push({ timestamp, value });
+      }
+    });
+  });
+
+  // Convert the object to an array of objects
+  const result = DASHBOARD_METRICS.map((metric) => ({
+    metricName: metric,
+    metricData: metricData[metric],
   }));
 
   return result;
