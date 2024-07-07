219949947d6c0ece9bb75ef38487c1161b19126b
Ettore Marangon <marangonettore@gmail.com>
2024-07-07 15:42:49 +0200

    added telemetry display component

--- frontend/src/Components/Content/DisplayTelemetry/DisplayTelemetry.jsx
+++ frontend/src/Components/Content/DisplayTelemetry/DisplayTelemetry.jsx
@@ -1,27 +1,67 @@
+import { useState, useEffect } from 'react';
+
+import { getGnBTelemetry } from '../../../Utils/fetching';
+import { transformTelemetryData } from '../../../Utils/transformData';
+import TelemetryItem from '../../TelemetryItem/TelemetryItem';
+import Loader from '../../Loader/Loader';
+
 import './DisplayTelemetry.css';
-import Graph from '../../Graph/Graph';
 
-const metrics = [
-  'dlBytes',
-  'dlMcs',
-  'dlBler',
-  'ulBytes',
-  'ulMcs',
-  'ulBler',
-  'ri',
-  'pmi',
-  'phr',
-  'pcmax',
-  'rsrq',
-  'sinr',
-];
+export default function DisplayTelemetry({ requestedData }) {
+  const [telemetryStatus, setTelemetryStatus] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-export default function DisplayTelemetry() {
+  useEffect(() => {
+    const fetchTelemetryData = async () => {
+      if (requestedData) {
+        try {
+          setIsLoading(true);
+
+          // TODO: update api call with ueId ...
+          const data = await getGnBTelemetry(
+            requestedData.startTime,
+            requestedData.endTime,
+            requestedData.devices[0].value
+          );
+
+          const processedData = transformTelemetryData(data);
+
+          console.log('processedData telemetry: ', processedData);
+
+          setTelemetryStatus(processedData);
+        } catch (err) {
+          console.error(err.message);
+        } finally {
+          setIsLoading(false);
+        }
+      } else {
+        // TODO: take out and build a warning message instead
+        // ...
+      }
+    };
+
+    fetchTelemetryData();
+  }, [requestedData]);
+
   return (
     <div className="contentTelemetry">
-      {metrics.map((m, i) => {
-        return <Graph metric={m} key={i} />;
-      })}
+      {isLoading && <Loader>Loading Data ...</Loader>}
+
+      {!isLoading && requestedData && (
+        <div className="items">
+          {telemetryStatus.map((m, i) => {
+            return (
+              <TelemetryItem
+                name={m.metricName}
+                rawData={m.metricData}
+                key={i}
+              />
+            );
+          })}
+
+          {/* TODO: add default display */}
+        </div>
+      )}
     </div>
   );
 }
