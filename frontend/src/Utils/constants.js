81917501d2af677a47697ad8983473778aa9e31a
Ettore Marangon <marangonettore@gmail.com>
2024-07-07 12:21:30 +0200

    added messages constants

--- frontend/src/Utils/constants.js
+++ frontend/src/Utils/constants.js
@@ -1,3 +1,5 @@
+// Dashboards modules and metrics
+
 export const CN5G_MODULES = [
   'amf',
   'ausf',
@@ -36,10 +38,19 @@ export const DASHBOARD_METRICS = [
   'puschSnr',
 ];
 
-/// APIs ///
+// API urls
 
 export const CN5G_BASE_URL = 'http://localhost:5173/api/cn5g/telemetry';
 
 export const GNB_LOGS_URL = 'http://localhost:5173/api/gnb/logs';
 
 export const GNB_TELEMETRY_URL = 'http://localhost:5173/api/gnb/telemetry';
+
+// Warning i.e., error messages
+
+export const EMPTY_MESSAGE = { type: null, text: '' };
+
+export const WARNING_TIMESPAN_MISSING = {
+  type: 'warning',
+  text: 'No timespan specified. Specify a valid timespan to dislay the data!',
+};
