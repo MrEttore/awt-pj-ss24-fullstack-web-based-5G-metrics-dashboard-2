fdd59ea45672eb8d994134249900f14bb64c5b57
Ettore Marangon <marangonettore@gmail.com>
2024-07-28 18:10:02 +0200

    added recent logs

--- frontend/src/components/Content/DisplayLogs/DisplayLogs.jsx
+++ frontend/src/components/Content/DisplayLogs/DisplayLogs.jsx
@@ -2,8 +2,11 @@ import { useState, useEffect } from 'react';
 
 import LogItem from '../../LogItem/LogItem';
 import Loader from '../../Loader/Loader';
-import Message from '../../Message/Message';
-import { getGnbLogs, getLiveGnbLogs } from '../../../utils/fetching';
+import {
+  getGnbLogs,
+  getLiveGnbLogs,
+  getRecentGnbLogs,
+} from '../../../utils/fetching';
 import { EMPTY_MESSAGE } from '../../../utils/constants';
 
 import './DisplayLogs.css';
@@ -24,7 +27,7 @@ export default function DisplayLogs({
     if (resetFlag) setLogsStatus([]);
   }, [resetFlag]);
 
-  // SET INITIAL UI MESSAGE
+  // SET INITIAL STATE
 
   useEffect(() => {
     if (!requestedData && !isLiveDataToggled) {
@@ -128,19 +131,53 @@ export default function DisplayLogs({
   }, [isLiveDataToggled, onMessage, isLiveDataLoading, logsStatus]);
 
   // FETCH RECENT DATA
-  // ...
+
+  useEffect(() => {
+    const fetchRecentData = async () => {
+      try {
+        setIsLoading(true);
+
+        const { recentData, error } = await getRecentGnbLogs();
+
+        if (error) throw new Error(error);
+
+        setLogsStatus(recentData);
+
+        console.log('UseEffect: Fetch recent data!');
+      } catch (err) {
+        onMessage({
+          type: 'error',
+          text: err.message,
+        });
+
+        setLogsStatus([]);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (requestedData || isLiveDataToggled) return;
+
+    fetchRecentData();
+  }, [onMessage, requestedData, isLiveDataToggled]);
 
   return (
     <div className={`contentLogs ${isLiveDataToggled ? 'live' : ''}`}>
       {isLoading && <Loader>Loading Logs ...</Loader>}
+
       {!isLoading && !requestedData && !isLiveDataToggled && (
-        <Message
-          message={{
-            type: 'info',
-            text: 'No logs to display.',
-          }}
-        />
+        <ul className="logs">
+          {logsStatus.map((log, i) => (
+            <LogItem
+              time={log.timestamp}
+              payload={log.payload}
+              logNr={i}
+              key={i}
+            />
+          ))}
+        </ul>
       )}
+
       {!isLoading && (requestedData || isLiveDataToggled) && (
         <ul className="logs">
           {logsStatus.map((log, i) => (
